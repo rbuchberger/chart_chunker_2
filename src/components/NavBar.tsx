1e39bf67df9a2da7535cfc9c57ce8d86936b9664
Robert Buchberger <robert@buchberger.cc>
2023-02-21 12:50:26 +0100

    Create NavBar component and button CSS

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,70 @@
+import classNames from "classnames"
+import { FunctionComponent } from "react"
+import { useStore } from "../hooks/useStore"
+
+export const NavBar: FunctionComponent<{
+  right?: React.ReactNode
+  left?: React.ReactNode
+}> = ({
+  right = <div className="w-32" />,
+  left = <div className="w-32" />,
+}) => {
+  const { file, setFile } = useStore()
+
+  const enableNext = !!file
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0])
+    } else {
+      setFile(null)
+    }
+  }
+
+  return (
+    <div className="flex w-full flex-row justify-between gap-10">
+      {left}
+
+      <label htmlFor="file" className="flex flex-grow cursor-pointer flex-row">
+        <input
+          type="file"
+          id="file"
+          className="hidden"
+          onChange={handleChange}
+        />
+        <div
+          className={classNames(
+            "whitespace-nowrap rounded-l-md px-3 py-2 transition-colors",
+            {
+              "bg-yellow-600  hover:bg-yellow-500": !enableNext,
+              "bg-gray-500 text-gray-50 hover:bg-gray-600": enableNext,
+            }
+          )}
+        >
+          Choose File
+        </div>
+
+        <div
+          className={classNames(
+            "col-span-4 flex flex-grow flex-row items-center rounded-r-md border border-l-0 px-3 font-mono",
+            {
+              "border-gray-500": enableNext,
+              "border-yellow-600": !enableNext,
+            }
+          )}
+        >
+          <div>{file ? file.name : "No file"}</div>
+        </div>
+      </label>
+
+      {right}
+    </div>
+  )
+}
+
+// <Link
+//   to="/"
+//   className="min-w-[8rem] overflow-hidden overflow-ellipsis rounded-md border border-gray-50 px-6 py-2 text-center font-mono text-3xl font-light"
+// >
+//   {file.name}
+// </Link>
