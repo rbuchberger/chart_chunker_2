3c1d7d57f66738c2440140236e9df303142ca6f4
Robert Buchberger <robert@buchberger.cc>
2023-02-21 12:57:32 +0100

    Simplify & improve options page
    - Stop using separate Options Page and OptionsHandler components
    - Use NavBar Component

--- src/pages/Options.tsx
+++ src/pages/Options.tsx
@@ -1,24 +1,122 @@
-import { FunctionComponent } from "react"
+import { ChangeEvent, FunctionComponent, useCallback } from "react"
 import { Link } from "react-router-dom"
-import { OptionsHandler } from "../components/OptionsHandler"
-import { useLoading } from "../hooks/useLoading"
+import { useStore } from "../hooks/useStore"
+import { ColumnSelectBox } from "../primitives/ColumnSelectBox"
+import { ChunkerPreview } from "../components/ChunkerPreview"
+import { NavBar } from "../components/NavBar"
 
 export const Options: FunctionComponent = () => {
-  switch (useLoading()) {
-    case "none":
-      return (
-        <p>
-          No file,{" "}
-          <Link className="text-yellow-500" to="/">
-            go back.
-          </Link>
-        </p>
+  const { config, setConfig, parser, file, chunkWorker } = useStore()
+
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLSelectElement>) => {
+      setConfig({
+        ...config,
+        [event.target.name]: event.target.value,
+      })
+    },
+    [config]
+  )
+
+  const handleColumnToggle = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const index = parseInt(
+        event.currentTarget.getAttribute("data-index") || ""
       )
-    case "reading":
-    case "parsing":
-      return <p>Loading...</p>
-    case "chunking":
-    case "done":
-      return <OptionsHandler />
+
+      if (event.target.checked && !config.keptColumns.includes(index)) {
+        setConfig({
+          ...config,
+          keptColumns: config.keptColumns.concat(index),
+        })
+      } else {
+        setConfig({
+          ...config,
+          keptColumns: config.keptColumns.filter((x) => x !== index),
+        })
+      }
+    },
+    [config]
+  )
+
+  const getFull = () => {
+    setTimeout(() => chunkWorker.postMessage({ returnFull: true }), 600)
   }
+
+  // Loading state check on options page should prevent this, but let's keep
+  // TS happy
+  if (!file || !parser) return null
+
+  return (
+    <form className="flex flex-col items-center gap-10">
+      <NavBar
+        left={
+          <Link to="/" className="btn btn--nav btn--gray">
+            Back
+          </Link>
+        }
+        right={
+          <Link
+            to="/presenter"
+            onClick={getFull}
+            className="btn btn--nav btn--yellow"
+          >
+            Next
+          </Link>
+        }
+      />
+
+      <div className="grid gap-10 sm:grid-cols-3">
+        <ColumnSelectBox
+          name="splitBasis"
+          label="Detect cycles based on"
+          columns={parser.columnItems}
+          value={config.splitBasis}
+          onChange={handleChange}
+        />
+
+        <ColumnSelectBox
+          name="voltageColumn"
+          label="Voltage"
+          value={config.voltageColumn}
+          columns={parser.columnItems}
+          onChange={handleChange}
+        />
+
+        <ColumnSelectBox
+          name="spcColumn"
+          label="Specific Capacity"
+          value={config.spcColumn}
+          columns={parser.columnItems}
+          onChange={handleChange}
+        />
+      </div>
+
+      <ChunkerPreview />
+
+      <section className="flex flex-col gap-6 rounded-md bg-gray-600 p-7">
+        <h2 className="text-2xl">Which columns would you like to keep?</h2>
+
+        <ul className="grid gap-x-6 gap-y-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {parser.columns.map((col, index) => (
+            <li key={index} className="grow rounded-md bg-gray-600">
+              <label className="flex h-full cursor-pointer items-center whitespace-nowrap px-2 py-1 font-mono text-sm leading-5">
+                <input
+                  type="checkbox"
+                  className="rounded-full text-yellow-500 focus:ring-yellow-500"
+                  name={col}
+                  data-index={index}
+                  onChange={handleColumnToggle}
+                  checked={config.keptColumns.includes(index)}
+                />
+                <div className="select-none overflow-hidden overflow-ellipsis px-2 pt-1">
+                  {col}
+                </div>
+              </label>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </form>
+  )
 }
