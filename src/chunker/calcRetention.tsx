d985b3cbdb88169a0b207bc616018219d4e15299
Robert Buchberger <robert@buchberger.cc>
2023-03-01 14:07:35 +0100

    Fix charge efficiency & retention analysis
    Now that half-cycles are possible, we have to be a little smarter about
    finding the first & last capacities & voltages.

--- src/chunker/calcRetention.tsx
+++ src/chunker/calcRetention.tsx
@@ -1,16 +1,15 @@
 import { PartialCycle } from "./buildCycle"
 
 export function calcRetention(cycles: PartialCycle[], cycle?: PartialCycle) {
-  if (
-    !cycle?.discharge?.maxSpecificCapacity ||
-    !cycles[0].discharge?.maxSpecificCapacity
-  ) {
+  const startRetention = cycles.find(
+    (cycle) => cycle.discharge?.maxSpecificCapacity
+  )?.discharge?.maxSpecificCapacity
+
+  if (!cycle?.discharge?.maxSpecificCapacity || !startRetention) {
     return null
   }
 
-  const ratio =
-    cycle.discharge.maxSpecificCapacity /
-    cycles[0].discharge.maxSpecificCapacity
+  const ratio = cycle.discharge.maxSpecificCapacity / startRetention
 
   return Math.round(ratio * 10000) / 100
 }
