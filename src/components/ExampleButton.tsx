d4752a0f3663145ba095cc755a9edee6e97ed484
Robert Buchberger <robert@buchberger.cc>
2023-03-03 12:44:55 +0100

    Improve example data button
    - Add "done" state
    - Better styles, behavior, and animations

--- src/components/ExampleButton.tsx
+++ src/components/ExampleButton.tsx
@@ -1,49 +1,78 @@
-import { Battery100 } from "@styled-icons/heroicons-solid"
+import { Battery100, Check } from "@styled-icons/heroicons-solid"
 import Tippy from "@tippyjs/react"
 import classNames from "classnames"
-import { FunctionComponent, useCallback, useState } from "react"
+import {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react"
+import { FakeFile } from "../hooks/useChunker"
 import { useStore } from "../hooks/useStore"
 
 export const ExampleButton: FunctionComponent = () => {
-  const [loading, setLoading] = useState(false)
-  const { setText, setFile, flash } = useStore((state) => ({
+  const { file, setFile, setText, flash } = useStore((state) => ({
+    file: state.file,
     setText: state.setText,
     setFile: state.setFile,
     flash: state.flash,
   }))
 
+  const [loading, setLoading] = useState<"none" | "loading" | "done">("none")
+
+  useEffect(() => {
+    if (file && (file as FakeFile)?.fake) {
+      setLoading("done")
+    } else {
+      setLoading("none")
+    }
+  }, [file])
+
   const loadSample = useCallback(() => {
-    setLoading(true)
+    setLoading("loading")
     fetch("/sample1.txt")
       .then((res) => res.text())
       .then(setText)
+      .then(() => setFile({ name: "example1.txt", fake: true }))
       .catch((err) => {
         flash(err.message || "Could not load example data, sorry")
+        setLoading("none")
       })
-      .finally(() => setLoading(false))
-
-    setFile({ name: "example1.txt", fake: true })
   }, [])
+
+  const content = useMemo(() => {
+    switch (loading) {
+      case "none":
+        return "Example"
+      case "loading":
+        return (
+          <Battery100
+            size={24}
+            className="motion-safe:animate-bounce motion-reduce:animate-pulse"
+          />
+        )
+      case "done":
+        return <Check size={24} />
+    }
+  }, [loading])
 
   return (
     <Tippy
       content="Load sample data for testing & experimenting with the chunker"
       placement="bottom"
-      disabled={loading}
+      disabled={loading !== "none"}
     >
       <button
         onClick={loadSample}
-        disabled={loading}
+        disabled={loading !== "none"}
         className={classNames("btn btn--nav", {
-          "btn--loading": loading,
-          "btn--gray": !loading,
+          "btn--loading": loading === "loading",
+          "btn--disabled": loading === "done",
+          "btn--gray": loading === "none",
         })}
       >
-        {loading ? (
-          <Battery100 size={24} className="animate-pulse" />
-        ) : (
-          "Example"
-        )}
+        {content}
       </button>
     </Tippy>
   )
