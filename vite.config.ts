648dab6bc3239171bacca345559c2995fad89f41
Robert Buchberger <robert@buchberger.cc>
2023-03-14 10:09:48 +0100

    Update sentry config
    - Use ENV vars (configured in netlify dashboard)
    - Add vite plugin
    - Add source maps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from "vite"
+import { sentryVitePlugin } from "@sentry/vite-plugin"
 import react from "@vitejs/plugin-react-swc"
+import { fileURLToPath } from "url"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "robert-buchberger",
+      project: "chunker",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      include: ["./dist"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
