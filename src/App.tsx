6316851231409eac5dd58ba14cb7b53acae3bb62
Robert Buchberger <robert@buchberger.cc>
2023-03-14 10:54:47 +0100

    Sentry - add replay and feedback

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,35 @@ import { router } from "./router"
 if (import.meta.env.PROD) {
   Sentry.init({
     dsn: "https://32d706941cf04fee8bba0d3c38b07839@o4504719318450176.ingest.sentry.io/4504719320285184",
-    integrations: [new BrowserTracing(), new CaptureConsole()],
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
+    // If the entire session is not sampled, use the below sample rate to sample
+    // sessions when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
 
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
+
+    beforeSend(event) {
+      // Check if it is an exception, and if so, show the report dialog
+      if (event.exception) {
+        Sentry.showReportDialog({
+          title:
+            "Something broke, sorry! You can help me fix it by sending a report.",
+          eventId: event.event_id,
+        })
+      }
+      return event
+    },
+
+    integrations: [
+      new BrowserTracing(),
+      new CaptureConsole(),
+      new Sentry.Replay(),
+    ],
   })
 }
 
